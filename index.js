767e61482aa98737f6fefc39cba660d9f250fb35
Heneros <punk60826@gmail.com>
2022-11-27 00:05:53 +0200

    userController

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+
 
 import { DB_PASSWORD } from './keys.js';
 import { registerValidation } from './validations/auth.js';
-import { validationResult } from 'express-validator';
-import UserModel from './models/User.js';
+
 import checkAuth from './utils/checkAuth.js';
+// import { register, login, getMe } from './controllers/UserController';
+
+//Все методы сохранить в UserController
+import { UserController } from './controllers/index.js';
 
 const app = express();
 
@@ -24,109 +26,17 @@ app.get('/', (req, res) => {
     res.send('Hello World1');
 });
 
-app.post('/auth/login', async (req, res) => {
-    try {
-
-        //Check user
-        const user = await UserModel.findOne({ email: req.body.email });
-        if (!user) {
-            return res.status(404).json({
-                message: "User not found"
-            });
-        }
-        //Check password
-        const inValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-        if (!inValidPass) {
-            return res.status(400).json({
-                message: "Password or user are wrong"
-            });
-        }
-
-        const token = jwt.sign(
-            {
-                _id: user._id, //must fit with id in database, 
-            },
-            'secret123',
-            {
-                expiresIn: '30d' //token stop be valid
-            },
-        );
-
-        const { passwordHash, ...useData } = user._doc;
-
-        res.json({
-            ...useData,
-            token
-        })
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Failed to authorize.',
-        });
-    }
-});
+app.post('/auth/login', UserController.login);
 
 
 
 /// check if fields before starts registerValidation
-app.post('/auth/register', registerValidation, async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array());
-        }
-
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
-
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName: req.body.fullName,
-            avatarUrl: req.body.avatarUrl,
-            passwordHash: hash,
-        });
-
-        const user = await doc.save();
-
-        const token = jwt.sign(
-            {
-                _id: user._id, //must fit with id in database, 
-            },
-            'secret123',
-            {
-                expiresIn: '30d' //token stop be valid
-            },
-        );
-
-
-        const { passwordHash, ...useData } = user._doc;
-
-        res.json({
-            ...useData,
-            token
-        })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Failed to register.',
-        });
-    }
-});
+app.post('/auth/register', registerValidation, UserController.register);
 
 
 ///checkAuth проверяет можно ли возвращать некоторые данные
 ///checkAuth сначало выполняется функция. а потом остальное
-app.get('/auth/me', checkAuth, (req, res) => {
-    try {
-        res.json({
-            success: true
-        });
-    } catch (err) {
-
-    }
-})
+app.get('/auth/me', checkAuth, UserController.getMe)
 
 
 app.listen(4444, (err) => {
--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -5,6 +5,16 @@ export default (req, res, next) => {
     ///Передавать токе без слова Bearer.
     const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
     if (token) {
+        ///Расшифровать токен
+        try {
+            const decoded = jwt.verify(token, 'secret123');
+            req.userId = decoded._id;
+            next();
+        } catch (e) {
+            return res.status(403).json({
+                message: 'No access',
+            });
+        }
 
     } else {
         ///Return тут чтобы два раза не передавался код
@@ -13,5 +23,5 @@ export default (req, res, next) => {
         });
     }
     // console.log(token);
-    res.send(token)
+    // res.send(token)
 }
