9f7399b2dbb2be734d82ee4f55413ef254fe9596
Heneros <punk60826@gmail.com>
2022-12-25 01:19:24 +0200

    Post schema updated

--- models/Post.js
+++ models/Post.js
@@ -1,23 +1,33 @@
 import mongoose from 'mongoose';
 
 const PostSchema = new mongoose.Schema({
-    fullName: {
+    title: {
         type: String,
         required: true,
     },
-    email: {
+    text: {
         type: String,
         required: true,
         unique: true
     },
-    passwordHash: {
-        type: String,
+    tags: {
+        type: Array,
+        default: []
+    },
+    viewsCount: {
+        type: Number,
+        default: 0
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", ///Ссылаться на отдельную модель
         required: true
     },
-    avatarUrl: String
-}, {
-    timestamps: true,
+    imageUrl: String
 },
+    {
+        timestamps: true,
+    },
 );
 
 export default mongoose.model('Post', PostSchema);
