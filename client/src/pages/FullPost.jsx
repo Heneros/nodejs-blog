d9211fffe16e15548b1e8e519c26255a46f8ca53
Heneros <punk60826@gmail.com>
2023-01-15 00:07:18 +0200

    login page

--- client/src/pages/FullPost.jsx
+++ client/src/pages/FullPost.jsx
@@ -4,7 +4,7 @@ import { Post } from "../components/Post";
 import { Index } from "../components/AddComment";
 import { CommentsBlock } from "../components/CommentsBlock";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import axios from "../axios";
 
 export const FullPost = () => {
   const { id } = useParams();
@@ -34,7 +34,7 @@ export const FullPost = () => {
       <Post
         id={data._id}
         title={data.title}
-        imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+        imageUrl={data.imageUrl}
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
